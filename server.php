3f6b7d0db53bbbca87637eac2c9945b890499da4
Samith Basnayaka <bmsd.basnayaka@gmail.com>
2017-06-10 22:56:02 +0530

    add the streaming server

---
+++ server.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+<script type="text/javascript" src="https://hosted.muses.org/mrp.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'http://213.239.218.99:7017/stream',
+'codec':'aac',
+'volume':100,
+'autoplay':false,
+'buffering':5,
+'title':'sawiya',
+'bgcolor':'#FFFFFF',
+'skin':'mcclean',
+'width':180,
+'height':60
+});
+</script>
+<!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+</body>
+</html>
